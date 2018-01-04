d92b569c2d36c6af3c778ab355cace867b0a1fd8
Tommy Ryanto <tommyryanto@icloud.com>
2018-01-04 16:39:20 +0700

    search php added

---
+++ search.php
@@ -0,0 +1,8 @@
+<?php
+	$temp=[];
+	$search= $_REQUEST['q'];
+	$json = json_decode(file_get_contents("http://mcc-ws-odd1718.herokuapp.com/books.php?q=".$search));
+	$temp = $json;
+	header("Content-type: 	application/json");
+	print json_encode($temp);
+?>
