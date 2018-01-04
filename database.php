ee0e0e9dd70ded9326edc63802578314f9dd5db0
Tommy Ryanto <tommyryanto@icloud.com>
2018-01-04 11:33:07 +0700

    some databse.php added

--- database.php
+++ database.php
@@ -1,4 +1,24 @@
 <?php
-	
+	header("Access-Control-Access: *");
 
+	$title = $_POST["bookTitle"];
+	$author = $_POST["bookAuthor"];
+	$genre = $_POST["bookGenre"];
+
+	$conn = new mysqli("localhost", "db");
+	if($conn->connect_error){
+		die("Connection failed!");
+	}
+
+	$insert = "INSERT INTO books (id, title, author, genre) VALUES(NULL, '$title'
+	, '$author', '$genre')";
+
+	if($conn->query($insert) === TRUE ){
+		echo "New data inputed!";
+	}
+	else{
+		echo "Error";
+	}
+
+	$conn->close();
 ?>
