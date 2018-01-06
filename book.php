7db666a88dc3f6bc46e4422f0497b44c51046c93
Tommy Ryanto <tommyryanto@icloud.com>
2018-01-06 13:21:42 +0700

    updated with own database

--- book.php
+++ book.php
@@ -1,9 +1,30 @@
+
 <?php 
 	
 	 header("Access-Control-Allow-Origin: *");
-	$book_list = [];
-	$json = json_decode(file_get_contents("http://mcc-ws-odd1718.herokuapp.com/books.php"));
-	$book_list = $json;
-	header('Content-Type: application/json');
-	print json_encode($book_list);
-?>
+	 $server = "sql12.freemysqlhosting.net";
+$username = "sql12214303";
+$pass = "2lyvrwYxyM";
+$db = "sql12214303";
+$connection = mysqli_connect($server, $username, $pass, $db);
+if ($_REQUEST) {
+$search_query = $_REQUEST['q'];
+$query_result = $connection->query("SELECT * FROM book WHERE title LIKE '%$search_query%' ");
+}else{
+	$query_result = $connection->query("SELECT * FROM book");
+}
+$book = [];
+$count = 0;
+$books = new stdClass();
+while ($data = $query_result->fetch_assoc()) {
+	$book[] = $data;
+	$count=$count+1;
+}
+$books->count = $count;
+if ($_REQUEST) {
+$books->q = $search_query;
+}
+$books->book= $book;
+header('Content-Type: application/json');
+print json_encode($books);
+	?>
